63d8da743de9634baa3991849bafdd3f33cb77d0
Abdellatif Beldjelti <a.beldjelti@esi-sba.dz>
2020-07-15 21:13:14 +0100

    Creating "navbar" and "books" components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BooksComponent } from './components/books/books.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BooksComponent
   ],
   imports: [
     BrowserModule,
